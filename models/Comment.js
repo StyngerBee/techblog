9edce75f6eaf46de0961608f1c51b97c597169b2
Curran Duke <cdsonics@yahoo.com>
2022-12-04 21:54:45 -0500

    finished out comment model

--- models/Comment.js
+++ models/Comment.js
@@ -7,5 +7,26 @@ Comment.init(
         id: {type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true},
 
         comment: { type: DataTypes.TEXT, allowNull: false},
+
+        createdAt: {type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW},
+
+        postedId: { type: DataTypes.INTEGER,
+        references: {
+            model: 'posted',
+            key: 'id'
+        }},
+        user_id: {type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        }},
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        
     }
-)
+);
+
+module.exports = Comment;
